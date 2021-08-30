e74b2a1ee07a3b3f584a3c38ddc43ec5b534b1fb
Debosmita Bhattacharjee <78953484+theLIT02@users.noreply.github.com>
2021-08-30 08:20:13 +0530

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,11 @@
+var food = [
+    { name: 'Biriyani', price : 120, quantity: 2},
+    { name: 'pasta', price : 50, quantity: 1},
+    { name: 'maggi', price : 10, quantity: 21},
+    { name: 'chilli chicken', price : 150, quantity: 1},
+
+];
+
+for(let i=0; i<food.length;i++){
+   console.log(food[i].name,food[i].price,food[i].quantity)
+}
